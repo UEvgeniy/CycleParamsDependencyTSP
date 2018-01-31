5d8de9fdff9ca1570a677b43690c245f535ae31f
Evgeniy Urnyshev <uevgenii@yandex.ru>
2018-01-31 17:56:25 +0300

    Revert "Revert "model and io packages + gitignore update""
    This reverts commit dd9c48d31286e2dbef3812173a8529f884c96733.

---
+++ app/src/io/TxtDatasetLoader.java
@@ -0,0 +1,16 @@
+package io;
+
+import model.TSPMatrixDataset;
+
+public class TxtDatasetLoader implements TSPDatasetLoader {
+
+    public TxtDatasetLoader(){
+        // todo get directory url
+    }
+
+
+    @Override
+    public TSPMatrixDataset load() {
+        return null;
+    }
+}
